b16ea3bd72a56e09ba3a6fc7ff3b525051d8dc83
Regun2 <132951178+Regun2@users.noreply.github.com>
2025-12-18 15:44:34 +0000

    remove comments

--- lua/entities/terminator_nextbot_zambieui/shared.lua
+++ lua/entities/terminator_nextbot_zambieui/shared.lua
@@ -17,13 +17,11 @@ if CLIENT then
     language.Add( "terminator_nextbot_zambieui", ENT.PrintName )
 
     function ENT:AdditionalClientInitialize()
-        -- White color
         local myColor = Vector( 1, 1, 1 )
         self.GetPlayerColor = function()
             return myColor
         end
         
-        -- Particle emitter setup
         self._particleEmitter = ParticleEmitter( self:GetPos() )
         self._nextParticle = 0
     end
@@ -33,9 +31,8 @@ if CLIENT then
             BaseClass.Think( self )
         end
         
-        -- Spawn white particles around the zombie (less frequently than elite)
         if self._particleEmitter and CurTime() >= self._nextParticle then
-            self._nextParticle = CurTime() + 0.08 -- Much less frequent than elite (0.015)
+            self._nextParticle = CurTime() + 0.08
             
             local pos = self:WorldSpaceCenter()
             local offset = VectorRand() * math.Rand( 10, 30 )
@@ -48,7 +45,7 @@ if CLIENT then
                 particle:SetDieTime( math.Rand( 0.4, 0.8 ) )
                 particle:SetStartAlpha( 255 )
                 particle:SetEndAlpha( 0 )
-                particle:SetStartSize( math.Rand( 2, 4 ) ) -- Slightly smaller than elite
+                particle:SetStartSize( math.Rand( 2, 4 ) )
                 particle:SetEndSize( 0 )
                 particle:SetColor( 255, 255, 255 )
                 particle:SetAirResistance( 100 )
@@ -60,7 +57,6 @@ if CLIENT then
     function ENT:Draw()
         self:DrawModel()
         
-        -- White glow
         local dlight = DynamicLight( self:EntIndex() )
         if dlight then
             dlight.pos = self:WorldSpaceCenter()
@@ -83,7 +79,6 @@ if CLIENT then
     return
 end
 
--- Stats
 ENT.SpawnHealth = 5000
 ENT.HealthRegen = 5
 ENT.HealthRegenInterval = 2
@@ -99,10 +94,9 @@ ENT.zamb_MeleeAttackSpeed = 2
 ENT.TERM_MODELSCALE = function() return math.Rand( 1.00, 1.05 ) end
 ENT.MyPhysicsMass = 85
 
--- Dodge mechanics
-ENT.UI_DODGE_CHANCE = 75 -- 75% chance to dodge
-ENT.UI_DODGE_COOLDOWN = 0.5 -- Minimum time between dodges
-ENT.UI_DODGE_DISTANCE = 500 -- Bigger dodge distance
+ENT.UI_DODGE_CHANCE = 75
+ENT.UI_DODGE_COOLDOWN = 0.5
+ENT.UI_DODGE_DISTANCE = 500
 ENT.UI_COLOR = Color( 255, 255, 255 )
 
 function ENT:AdditionalInitialize()
@@ -129,46 +123,36 @@ function ENT:AdditionalInitialize()
     self.FallDamagePerHeight = 0.05
     self.DeathDropHeight = 1500
     
-    -- Dodge tracking
     self._lastDodgeTime = 0
     self._dodgeCount = 0
 end
 
--- Dodge mechanic - Override damage
 function ENT:OnTakeDamage( dmg )
     local curTime = CurTime()
     
-    -- Check if we can dodge
     if curTime - self._lastDodgeTime >= self.UI_DODGE_COOLDOWN then
         local dodgeRoll = math.random( 1, 100 )
         
         if dodgeRoll <= self.UI_DODGE_CHANCE then
-            -- DODGE!
             self._lastDodgeTime = curTime
             self._dodgeCount = ( self._dodgeCount or 0 ) + 1
             
-            -- Visual feedback
             self:DodgeEffect( dmg:GetDamagePosition() )
             
-            -- Play dodge sound (whoosh/miss sound)
             self:EmitSound( "weapons/slam/throw.wav", 70, 150 )
             
-            -- Anger slightly from being attacked
             self:Anger( 5 )
             
-            -- Completely negate damage
             return
         end
     end
     
-    -- Call base class if didn't dodge
     BaseClass.OnTakeDamage( self, dmg )
 end
 
 function ENT:DodgeEffect( hitPos )
     if not hitPos then hitPos = self:WorldSpaceCenter() end
     
-    -- White flash effect
     local effectdata = EffectData()
     effectdata:SetOrigin( hitPos )
     effectdata:SetNormal( VectorRand() )
@@ -177,14 +161,11 @@ function ENT:DodgeEffect( hitPos )
     effectdata:SetRadius( 3 )
     util.Effect( "ManhackSparks", effectdata )
     
-    -- Quick dodge movement - BIGGER DISTANCE
     if self.loco:IsOnGround() then
         local dodgeDir = VectorRand()
         dodgeDir.z = 0
         dodgeDir:Normalize()
         
-        -- Bigger dash
         self.loco:SetVelocity( self.loco:GetVelocity() + dodgeDir * self.UI_DODGE_DISTANCE )
     end
-
 end
