06eb3f2765a80298c6e00a15032e00e990e02f15
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-04-19 18:29:17 -0700

    Sound tweaks
    Move away from god crab scream

--- lua/entities/terminator_nextbot_zambiereanimator/shared.lua
+++ lua/entities/terminator_nextbot_zambiereanimator/shared.lua
@@ -200,11 +200,11 @@ function ENT:AdditionalInitialize()
     self.term_SoundPitchShift = -30
     self.term_SoundLevelShift = 15
 
-    self.term_CallingSound = "npc/strider/striderx_die1.wav"
-    self.term_CallingSmallSound = "npc/stalker/go_alert2a.wav"
+    self.term_CallingSound = "npc/stalker/stalker_scream4.wav"
+    self.term_CallingSmallSound = "npc/stalker/stalker_scream1.wav"
     self.term_FindEnemySound = { "npc/headcrab_poison/ph_scream1.wav", "npc/headcrab_poison/ph_scream2.wav", "npc/headcrab_poison/ph_scream3.wav" }
     self.term_AttackSound = { "npc/fast_zombie/fz_scream1.wav" }
-    self.term_AngerSound = "NPC_FastZombie.Alert"
+    self.term_AngerSound = "NPC_BlackHeadcrab.Threat"
     self.term_DamagedSound = { "npc/headcrab_poison/ph_pain1.wav", "npc/headcrab_poison/ph_pain2.wav", "npc/headcrab_poison/ph_pain3.wav" }
     self.term_DieSound = "npc/zombie_poison/pz_die1.wav"
     self.term_JumpSound = "npc/zombie_poison/pz_left_foot1.wav"
