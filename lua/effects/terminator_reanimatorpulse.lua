757c4f7ae305cbb462f5e712f47757b40c54499e
Regun2 <132951178+Regun2@users.noreply.github.com>
2026-04-21 21:16:24 +0100

    redesigned pulse effect

--- lua/effects/terminator_reanimatorpulse.lua
+++ lua/effects/terminator_reanimatorpulse.lua
@@ -1,4 +1,3 @@
-
 AddCSLuaFile()
 
 function EFFECT:Init( effectData )
@@ -10,11 +9,13 @@ function EFFECT:Init( effectData )
 
     self:SetModel( "models/hunter/misc/sphere2x2.mdl" )
     self:SetRenderMode( RENDERMODE_TRANSADD )
-    self:SetMaterial( "lights/white002" )
+    self:SetMaterial( "reanimator/reanimator_orb" )
     self:SetModelScale( 0 )
 
     self:SetPos( self.origin )
 
+    self.matOverlay = Material( "reanimator/reanimator_orb" )
+    self.spinAngle = 0
 end
 
 function EFFECT:Think()
@@ -26,18 +27,35 @@ function EFFECT:Think()
     self:SetModelScale( newScale )
     self:SetColor( Color( self.R_color, 0, 0, A_value ) )
 
+    self.spinAngle = ( self.spinAngle - FrameTime() * 120 ) % 360
+    local ang = self:GetAngles()
+    ang.y = self.spinAngle
+    self:SetAngles( ang )
+
     if self:GetModelScale() > self.size / 128 then
         return false
-
     else
         return true
-
     end
 end
 
 function EFFECT:Render()
-    render.CullMode( MATERIAL_CULLMODE_NONE )
-    self:DrawModel()
-    render.CullMode( MATERIAL_CULLMODE_CCW )
+    local scrollSpeed = 0.5
+    local offset = ( CurTime() * scrollSpeed ) % 1
 
+    local mat = self.matOverlay
+    if mat and not mat:IsError() then
+        local matrixT = Matrix()
+        matrixT:Translate( Vector( 0, offset, 0 ) )
+
+        mat:SetMatrix( "$basetexturetransform", matrixT )
+    end
+
+    render.CullMode( MATERIAL_CULLMODE_NONE )
+    render.MaterialOverride( mat )
+
+    self:DrawModel()
+
+    render.MaterialOverride( nil )
+    render.CullMode( MATERIAL_CULLMODE_CCW )
 end
